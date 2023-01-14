35274e00f886f2a9d641dac657971a3237f73e8f
Open Sudo <open-sudo@voldemort.lord>
2023-01-14 15:37:04 -0500

    Add camel k operator

---
+++ delete-all.sh
@@ -0,0 +1,16 @@
+aws cloudformation delete-stack --stack-name rosa-idp-cw-logs
+aws cloudformation delete-stack --stack-name rosa-idp-iam-external-secrets
+aws cloudformation delete-stack --stack-name rosa-idp-ecr
+aws cloudformation delete-stack --stack-name rosa-idp-cw-metrics-credentials
+
+
+oc delete Application root-application -n openshift-gitops
+oc delete ArgoCD -n openshift-gitops
+
+oc delete ClusterRoleBinding openshift-gitops-argocd-application-controller -n openshift-gitops
+oc delete ClusterRoleBinding openshift-gitops-applicationset-controller -n openshift-gitops
+oc delete ClusterRoleBinding openshift-gitops-argocd-server -n openshift-gitops
+
+oc delete ClusterRoleBinding openshift-gitops-cluster-admin
+
+
