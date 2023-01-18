ba5f6b11de4457f289a557577d37e088912b278e
Open Sudo <open-sudo@voldemort.lord>
2023-01-18 10:56:17 -0500

    fix bug in delete script

--- delete-all.sh
+++ delete-all.sh
@@ -3,7 +3,7 @@ aws cloudformation delete-stack --stack-name rosa-idp-iam-external-secrets
 aws cloudformation delete-stack --stack-name rosa-idp-ecr
 aws cloudformation delete-stack --stack-name rosa-idp-cw-metrics-credentials
 aws cloudformation delete-stack --stack-name rosa-idp-rds-shared-instance-credentials
-aws cloudformation delete-stack --stack-name rosa-idp-rds-inventory-credentials]
+aws cloudformation delete-stack --stack-name rosa-idp-rds-inventory-credentials
 aws cloudformation delete-stack --stack-name rosa-idp-iam-external-secrets-rds
 
 
